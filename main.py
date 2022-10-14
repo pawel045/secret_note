44b04fc03534a2fe606dfcb418adb4caa37e990b
pawel <proszczyk96@gmail.com>
2022-10-14 14:13:07 +0200

    .

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ class Notes(db.Model):
     recipient = db.Column(db.String(100))
     user_id = db.Column(db.ForeignKey('users.id'))
 
-# db.create_all()
+db.create_all()
 #
 # new_user = Users(
 #     login='pawel',
